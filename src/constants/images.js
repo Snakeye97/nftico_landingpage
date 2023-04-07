ad1693e6490744d9184c52b12833b99b144fd789
nilesh9766814824 <nileshchoudhary585@gmail.com>
2023-04-07 08:52:11 +0530

    .

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,20 @@
 import menu from '../assets/menu.svg';
 import close from '../assets/close.svg';
 import logo from '../assets/logo.svg';
+import Homeimg1 from '../assets/HomeImg 1.png';
+import play from '../assets/play.svg';
+import arrow from '../assets/arrow.svg';
+import HomeHexa from '../assets/HomeHexa.svg';
+import Round1Img from '../assets/Roundimg-1.jpg'
 
 
 export default {
   menu,
   close,
-  logo
+  logo,
+  Homeimg1,
+  play,
+  arrow,
+  HomeHexa,
+  Round1Img
 }
