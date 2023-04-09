9d1ea5fc806d16eb325999189e280927608563ca
Nilesh <117421926+Snakeye97@users.noreply.github.com>
2023-04-09 23:12:50 +0530

    Update Subheading.jsx

--- src/components/Subheading.jsx
+++ src/components/Subheading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Subheading = ({title,text}) => {
   return (
     <div className='flex flex-col items-center justify-center relative'>
-      <h1 className='bg-gradient-to-r from-blue to-pinkHover text-transparent bg-clip-text font-Outfit font-bold leading-[130px] text-[104px]'>{title}</h1>
+      <h1 className='bg-gradient-to-r from-blue to-pinkHover text-transparent bg-clip-text font-Outfit font-bold leading-[130px] md:text-[104px] text-[74px]'>{title}</h1>
       <p className='text-whiteText font-Outfit text-[40px] absolute font-semibold top-[45%]'>{text}</p>
     </div>
   )
