2a1502ca18921864a980370bd1676c445c0ad09f
nilesh9766814824 <nileshchoudhary585@gmail.com>
2023-04-10 12:00:35 +0530

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Navbar from './Navbar';
 import roundImg from "./roundImg";
 import Subheading from './Subheading';
-import DropList from './Droplist';
 
 export {
   Navbar,
   roundImg,
-  Subheading,
-  DropList
+  Subheading
 };
