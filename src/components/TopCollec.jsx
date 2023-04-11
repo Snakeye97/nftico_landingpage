4de0a6890760593610f446e00b5475a585ee26fa
nilesh9766814824 <nileshchoudhary585@gmail.com>
2023-04-11 19:38:12 +0530

    .

--- src/components/TopCollec.jsx
+++ src/components/TopCollec.jsx
@@ -5,21 +5,23 @@ import images from '../assets/images'
 const TopCollec = () => {
   return (
     <div>
-      <div className='flex flex-wrap items-center justify-center m-[4rem] gap-6'>
+      <div className='flex flex-wrap items-center justify-center m-[4rem] gap-4'>
         {topCollection.map((Top)=> (
-          <div key={Top.id}
-            className='w-[320px] h-[80px] text-whiteText flex gap-3 bg-cardBg rounded-[8px] items-center justify-center top__border'>
-            <img src={Top.Image} alt="image" className='w-[50px] h-[50px] rounded-full '/>
+          <div className='p-[1px] w-[350px] h-[80px] bg-transparent hover:bg-gradient-to-r from-blue to-pinkHover rounded-[10px]'>
+            <div key={Top.id}
+              className='  text-whiteText w-full h-full flex gap-3 bg-cardBg rounded-[10px] items-center justify-start p-[1rem]'>
+              <img src={Top.Image} alt="image" className='w-[50px] h-[50px] rounded-full '/>
 
-            <div>
-              <div className='flex justify-between]'>
-                <h2>{Top.title}</h2>
-                <p>{Top.text}</p>
-              </div>
+              <div className='gap-2 flex flex-col'>
+                <div className='flex items-center'>
+                  <h2 className='font-Outfit text-[16px] w-[200px]'>{Top.title}</h2>
+                  <p className='font-Outfit text-[12px] w-[50px] text-[#03DB80]'>{Top.text}</p>
+                </div>
 
-              <div className='flex justify-between'>
-                <p className='flex'><img src={images.Flow} alt="" />{Top.flow}</p>
-                <p>{Top.percent}</p>
+                <div className='flex justify-between'>
+                  <p className='flex font-Outfit text-[12px] gap-1'><img src={images.Flow} alt="" />{Top.flow}</p>
+                  <p className='font-Outfit text-[12px]'>{Top.percent}</p>
+                </div>
               </div>
             </div>
           </div>
